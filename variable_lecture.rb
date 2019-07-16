fdb0dc3be47c46f876f052aadd88504ae5a0c16d
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2019-07-16 15:32:28 -0700

    This commit updates variable_lecture with the final example

--- variable_lecture.rb
+++ variable_lecture.rb
@@ -1,5 +1,27 @@
 # Variables
 
-my_num = 42
-my_num = 42 + 8
-puts my_num
+# my_num = 42
+# my_num = 42 + 8
+# puts my_num
+
+
+# count = 10
+# count += 1 # same as 'count = count + 1'
+# puts count
+
+# num = 12
+# is_even = num % 2 == 0
+# puts is_even
+
+# Declare a variable named 'firstName' & assign it to your first name as a string:
+
+firstName = "Ozzy"
+
+#Declare a variable named 'lastName' and assign it your last name as a string:
+
+lastName = "Lievano"
+
+#Declare a variable named 'fullName' and assign it the concatenation of 'fN' & 'lN'
+
+fullName = firstName + " " + lastName
+puts fullName
