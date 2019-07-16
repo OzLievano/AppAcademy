57799c94e29c2ea4a939c15d4dd8b97ed56319b0
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2019-07-16 15:33:22 -0700

    This commit adds methods lecture to the repo

---
+++ methods_lecture1.rb
@@ -0,0 +1,13 @@
+# Methods
+
+#defining a method
+def message
+
+    puts "Hey Programmers!"
+    puts "What's your lunch?"
+
+end
+
+#Call a method
+
+message
---
+++ methods_lecture2.rb
@@ -0,0 +1,8 @@
+# Methods
+
+def say_hello(person1,person2)
+  puts "Hello! " + person1 + " and " + person2 + "."
+end
+
+say_hello("Jacob","Nancy")
+say_hello("Claire", "Joy")
