8ae74720124fad1fb7012c9fa9473aceb1884d55
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2019-07-16 20:21:47 -0700

    This commit adds the average of three example

---
+++ average_of_three.rb
@@ -0,0 +1,10 @@
+def average_of_three(num1,num2,num3)
+  sum = num1 + num2 + num3
+  avg = sum / 3.0
+  return avg
+end
+
+
+puts average_of_three(3,7,8)
+puts average_of_three(2,5,17)
+puts average_of_three(2,8,1)
---
+++ methods_lecture3.rb
@@ -0,0 +1,10 @@
+#Methods
+
+def calc_avg(num1,num2)
+    sum = num1 + num2
+    avg = sum / 2.0
+    return avg
+end
+
+# the return keyword lets a method call evaluate to a value
+result = puts calc_avg(5,10)
